5c7bd884269745c3a7209e28d003f4ead4599130
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-29 16:27:50 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+pwd = input("what is the master password? ")
+
+#user is picking wheter they want to add or view previous passwords 
+while True:
+    mode = input("would you like to add new passwords or view previous passwords? (view/add)? ")
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid mode. ")
+
