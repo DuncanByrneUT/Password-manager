9779f6ff769e083a10c3f6af86ba0e9eb89c2015
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-29 16:34:17 -0500

    added the quit function

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 pwd = input("what is the master password? ")
 
-#user is picking wheter they want to add or view previous passwords 
+#user is picking wheter they want to add or view previous passwords
 while True:
-    mode = input("would you like to add new passwords or view previous passwords? (view/add)? ")
+    mode = input("would you like to add new passwords or view previous passwords? (view/add)? Press Q to quit").lower()
+    if mode == "q":
+        break
     if mode == "view":
         pass
     elif mode == "add":
         pass
     else:
         print("Invalid mode. ")
+        continue
 
