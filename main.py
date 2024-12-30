396d1afda67f1f8b55a15e3ca74f1c52258e62dd
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-29 19:40:33 -0500

    added functions outside and inside the while loop

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 pwd = input("what is the master password? ")
 
+# functions that we will pass into the while loop
+def view():
+    pass
+def add():
+    name = input('Account Name: ')
+
+
 #user is picking wheter they want to add or view previous passwords
 while True:
     mode = input("would you like to add new passwords or view previous passwords? (view/add)? Press Q to quit").lower()
     if mode == "q":
         break
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
-        pass
+        add()
     else:
         print("Invalid mode. ")
         continue
