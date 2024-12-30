07a8e1137a99fa28a063da38cdd5add2422e4b76
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-29 20:49:24 -0500

    added the options for the user to store their names and passwords

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-pwd = input("what is the master password? ")
+
+master_pwd = input("what is the master password? ")
 
 # functions that we will pass into the while loop
+import os
+
 def view():
-    pass
+    if os.path.exists('passwords.txt'):
+        with open('passwords.txt', 'r') as f:
+            for line in f:
+                try:
+                    data = line.rstrip()
+                    user, passw = data.split("|")
+                    print("User:", user, "Password:", passw)
+                except ValueError:
+                    print("Error: Incorrect format in file.")
+    else:
+        print("No password file found.")
+
+
+
 def add():
     name = input('Account Name: ')
-    acct_pwd = input("Password: ")
+    pwd = input("Password: ")
 
     # with helps us open and close the file
     # with the 'a' mode, we are appending something to the end of the file, and will create a new one if none exist
     with open('passwords.txt', 'a') as f:
         # \n is telling the program to end the current line
-        f.write(name + "|" + acct_pwd +'\n')
+        f.write(name + "|" + pwd +'\n')
 
-
-
-
-#user is picking wheter they want to add or view previous passwords
 while True:
     mode = input("would you like to add new passwords or view previous passwords? (view/add)? Press Q to quit: ").lower()
     if mode == "q":
